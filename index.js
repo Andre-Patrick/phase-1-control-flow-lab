02f0979234020562332fd6147a5e1b815a8a1394
Patrick <patrick.andre@student.moringaschool.com>
2025-03-13 22:51:12 +0300

    Second Try

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   if (distance <= 400) {
     return 'This one is on me!';
-  }
-  else if (distance <= 2000) {
+  } else if (distance <= 2000) {
     return "That will be twenty bucks.";
-    else if (distance > 2000) {
-      return "I will gladly take your thirty bucks.";
-    else if (distance > 2500) {
-      return "No can do.";
-    }
+  } else if (distance > 2000 && distance <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (distance > 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   if (city === 'NYC') {
     return "Ok, sounds good.";
-    else {
-      return "No go.";
+  } else {
+    return "No go.";
   }
 }
 
-function switchOnCharmFromTip(){
-if (tip === "generous") {
-  return "Thank you so much.";
-  else if (tip === "not as generous") {
+function switchOnCharmFromTip(tip){
+  if (tip === "generous") {
+    return "Thank you so much.";
+  } else if (tip === "not as generous") {
     return "Thank you.";
-    else {
-      return "Bye.";
-    }
+  } else {
+    return "Bye.";
+  }
+}
